34f71dba5f15a3845bb074bedd4fc0b6810d8fa6
yutaro1031 <yutaro50@gmail.com>
2018-11-17 20:01:04 +0900

    almost fixed

--- bookLog/static/search_books.js
+++ bookLog/static/search_books.js
@@ -18,10 +18,17 @@ $("#searchForm").submit(function(){
                 //検索ヒット数分、要素を追加
                 data.items.forEach(function(val, j){
 
+                    //長い文字列を省く
+                    let titleText = data.items[j].volumeInfo.title;
+                    let authorsText = data.items[j].volumeInfo.authors;
+                    authorsText = authorsText.join(",");
+                    titleText = titleText.length > 15 ? (titleText).slice(0,15)+"…" : titleText;
+                    authorsText = authorsText.length > 14 ? (authorsText).slice(0,14)+"…" : authorsText;
+
                     //要素の作成
-                    let title = $('<h5 class="searched_title">' + data.items[j].volumeInfo.title + '</h5>');
-                    let authers = $('<p class="searched_authres">' + data.items[j].volumeInfo.authers + '</p>');
-                    let publishedDate = $('<p class="searched_publishedDate">' + data.items[j].volumeInfo.publishedDate + '</p>');
+                    let title = $('<p class="searched_title">' + titleText + '</p>');
+                    let authers = $('<p class="searched_authors">' + authorsText + '</p>');
+                    //let publishedDate = $('<p class="searched_publishedDate">' + data.items[j].volumeInfo.publishedDate + '</p>');
                     let thumbnailURL = '';
 
                     //サムネイル画像の取得
@@ -33,11 +40,13 @@ $("#searchForm").submit(function(){
 
                     //「本棚に追加」ボタンの実装(どうしてもFlask側で実装したい為、コードが汚くなった)
                     let singleQuote = "'";
-                    let addButton = $('<button onclick="location.href=' + singleQuote + '/add/' + data.items[j].id + singleQuote + '">本棚に追加する</button>');
+                    let addButton = $('<button class="btn btn-success" onclick="location.href=' + singleQuote + '/add/' + data.items[j].id + singleQuote + '">本棚に追加する</button>');
                     
                     //ページへ表示
-                    $("#result").append('<li class="searched_list"></li>');
-                    $(".searched_list").eq(j).append(title, authers, publishedDate, thumbnailURL, addButton);
+                    $("#row").append('<div class="searched_list col-md-3 col-sm-4 col-6"></div>');
+                    $(".searched_list").eq(j).append(thumbnailURL);
+                    $(".searched_list").eq(j).append('<div class="book_info"></div>');
+                    $(".book_info").eq(j).append(title, authers, addButton);
                 });
             } else {
                 //検索結果が見つからないときの処理(いずれ書く)
