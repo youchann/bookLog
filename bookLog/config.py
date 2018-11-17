0079d5960efe7037af98a4e57e9ecafb564cf233
yutaro1031 <yutaro50@gmail.com>
2018-11-17 22:28:12 +0900

    なぜかconfig.pyがなかった

---
+++ bookLog/config.py
@@ -0,0 +1,8 @@
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///bookLog.db'
+SECRET_KEY = 'secret key'
+
+# firebaseのconfig
+
+
+#Google Books APIに使うurl
+google_books_api_url = 'https://www.googleapis.com/books/v1/volumes?q=id:'
