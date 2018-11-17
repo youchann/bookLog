29198fc63b00c875850fe9d7af9cc2aab189a698
yutaro1031 <yutaro50@gmail.com>
2018-11-17 21:34:20 +0900

    configの修正

---
+++ bookLog/config.py
@@ -0,0 +1,11 @@
+
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///bookLog.db'
+SECRET_KEY = 'secret key'
+
+# firebaseのconfig
+
+
+#Google Books APIに使うurl
+google_books_api_url = 'https://www.googleapis.com/books/v1/volumes?q=id:'
+
