06416c982f581adc13ea064029a2f715571a47b7
yutaro1031 <yutaro50@gmail.com>
2018-12-19 20:13:15 +0900

    レスポンシブ対応

--- bookLog/__init__.py
+++ bookLog/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 # from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('bookLog.config')
+app.config.from_object('application.config')
 
 # db = SQLAlchemy(app)
 
-import bookLog.views
+import application.views
